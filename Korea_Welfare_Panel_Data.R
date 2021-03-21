c43059b9afb7b14bfe9e5a3777462b14fac7f0ee
sjh4773 <jihun950202@naver.com>
2021-03-21 23:06:49 +0900

    korea_welfare_panel_data

--- Korea_Welfare_Panel_Data.R
+++ Korea_Welfare_Panel_Data.R
@@ -1,10 +1,12 @@
+Sys.setlocale("LC_CTYPE", ".1251")
+
 library(foreign)
 library(dplyr)
 library(ggplot2)
 library(readxl)
 
 raw_welfare <- read.spss(file = "c:/R/Data/Koweps_hpc10_2015_beta1.sav",
-                         to.data.frame = T)
+                         to.data.frame = T, encoding= 'UTF-8')
 
 welfare <- raw_welfare
 
@@ -246,6 +248,8 @@ ggplot(data = job_female, aes(x = reorder(job, n), y = n)) +
 # 종교 변수 검토
 class(welfare$religion)
 
+welfare$religion <- ifelse(welfare$religion == 1, "yes", "no")
+
 table(welfare$religion)
 
 qplot(welfare$religion)
@@ -258,6 +262,7 @@ table(welfare$marriage)
 # 이혼 여부 변수 만들기
 welfare$group_marriage <- ifelse(welfare$marriage == 1, "marriage",
                                  ifelse(welfare$marriage == 3, "divorce", NA))
+
 
 table(welfare$group_marriage)
 
@@ -279,3 +284,134 @@ divorce <- religion_marriage %>%
   select(religion, pct)
 
 divorce
+
+# 이혼율 결과 그래프
+ggplot(data = divorce, aes(x = religion, y = pct)) + geom_col()
+
+
+
+# 연령대별 이혼율 표
+ageg_marriage <- welfare %>% filter(!is.na(group_marriage)) %>%
+  group_by(ageg, group_marriage) %>% summarise(n = n()) %>%
+  mutate(tot_group = sum(n)) %>% mutate(pct = round(n / tot_group * 100, 1))
+
+ageg_marriage
+
+# 초년 제외, 이혼 추출
+ageg_divorce <- ageg_marriage %>%
+  filter(ageg != "young" & group_marriage == "divorce") %>%
+  select(ageg, pct)
+
+ageg_divorce
+
+ggplot(data = ageg_divorce, aes(x = ageg, y = pct)) + geom_col()
+
+
+# 연령대, 종교 유무, 결혼 상태별 비율표 만들기
+ageg_religion_marriage <- welfare %>%
+  filter(!is.na(group_marriage) & ageg != "young") %>%
+  group_by(ageg, religion, group_marriage) %>%
+  summarise(n = n()) %>%
+  mutate(tot_group = sum(n)) %>%
+  mutate(pct = round(n/tot_group*100, 1))
+
+ageg_religion_marriage
+
+# 연령대 및 종교 유무별 이혼율 표 만들기
+df_divorce <- ageg_religion_marriage %>%
+  filter(group_marriage == "divorce") %>%
+  select(ageg, religion, pct)
+
+df_divorce
+
+ggplot(data = df_divorce, aes(x = ageg, y = pct, fill = religion)) +
+  geom_col(position = "dodge")
+
+
+# (지역별 연령대 비율 - "노년층이 많은 지역은 어디일까?")
+
+class(welfare$code_region)
+
+table(welfare$code_region)
+
+
+# 지역 코드 목록 만들기
+list_region <- data.frame(code_region = c(1,2,3,4,5,6,7), region = c("서울", "수도권(인천/경기)", "부산/경남/을산", "대구/경북", "대전,충남", "강원/충북", "광주/전남/전북/제주도"))
+
+list_region
+
+welfare <- left_join(welfare, list_region, id = "code_region")
+
+welfare %>%
+  select(code_region, region) %>%
+  head
+
+# 지역별 연령대 비율 분석하기
+region_ageg <- welfare %>%
+  group_by(region, ageg) %>%
+  summarise(n = n()) %>%
+  mutate(tot_group = sum(n)) %>%
+  mutate(pct = round(n/tot_group*100,2))
+
+
+head(region_ageg)
+
+
+# 그래프 만들기
+ggplot(data = region_ageg, aes(x = region, y = pct, fill = ageg)) +
+  geom_col() +
+  coord_flip()
+
+# 노년층 비율 내림차순 정렬
+list_order_old <- region_ageg %>%
+  filter(ageg == "old") %>%
+  arrange(pct)
+
+list_order_old
+
+
+# 지역명 순서 변수 만들기
+order <- list_order_old$region
+order
+
+
+# 그래프 만들기
+ggplot(data = region_ageg, aes(x = region, y = pct, fill = ageg)) +
+  geom_col() +
+  coord_flip() +
+  scale_x_discrete(limits = order)
+
+# 연령대 순으로 막대 색깔 나열하기
+# 앞에서 만든 그래프는 막대 색깔이 young, old, middle 순으로 나열되어 있다.
+# 이를 초년, 중년, 노년의 연령대 순으로 나열되도록 설정
+
+# ageg 변수는 character 타입이기 때문에 levels가 없다.
+class(region_ageg$ageg)
+
+levels(region_ageg$ageg)
+
+# factor()를 이용해 ageg 변수를 factor 타입으로 변환하고,
+# level 파라미터를 이용해 순서를 지정한다.
+region_ageg$ageg <- factor(region_ageg$ageg,
+                           level = c("old", "middle", "young"))
+
+class(region_ageg$ageg)
+
+levels(region_ageg$ageg)
+
+# 연령대별로 나열된 막대 그래프 출력
+ggplot(data = region_ageg, aes(x = region, y = pct, fill = ageg)) +
+  geom_col() +
+  coord_flip() +
+  scale_x_discrete(limits = order)
+
+
+
+# 그래프 막대 색깔이 연령대 순으로 나열되어 지역별 연령대 비율 구성을 쉽게 파악할 수 있음
+ggplot(data = region_ageg, aes(x = region, y = pct, fill = ageg)) +
+  geom_col() +
+  coord_flip() +
+  scale_x_discrete(limits = order)
+
+
+
